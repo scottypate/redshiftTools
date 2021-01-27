b04c90758de5d413cbb7a99730e3eb9747b7c948
scottypate <scotty.pate@auth0.com>
2021-01-27 04:33:40 -0600

    Remove using clause

--- R/upsert.R
+++ R/upsert.R
@@ -92,9 +92,8 @@ rs_upsert_table = function(
       keysCond = paste(table_name,".",keys, " IN (SELECT ",keys," FROM ", stageTable, " GROUP BY 1)", sep="")
       keysWhere = sub(" and $", "", paste0(keysCond, collapse="", sep=" and "))
       message(paste('delete from', table_name, 'where', keysWhere))
-      queryStmt(dbcon, sprintf('delete from %s using %s where %s',
+      queryStmt(dbcon, sprintf('delete from %s where %s',
               table_name,
-              stageTable,
               keysWhere
               ))
     }
